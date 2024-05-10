801159720a7c11595612230528bdd032797fb74f
Shawn Dsilva <zzzzshawn@gmail.com>
2024-05-11 00:43:07 +0530

    commit

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -9,7 +9,7 @@ const RootLayout = () => {
       <Topbar />
       <LeftSidebar />
 
-      <section className='flex flex-1 min-h-[80vh]'>
+      <section className='flex flex-1 md:h-screen min-h-[80vh]'>
         <Outlet />
       </section>
 
