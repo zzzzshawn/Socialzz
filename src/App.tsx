548fea6d36c2a6dbfd0f821ff40e106735440434
Shawn Dsilva <zzzzshawn@gmail.com>
2024-07-31 15:27:59 +0530

    lets see...

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
-import './globals.css';
-import { Routes, Route } from 'react-router-dom'
-import SigninForm from './_auth/forms/SigninForm';
-import { Allusers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
-import SignupForm from './_auth/forms/SignupForm';
-import AuthLayout from './_auth/AuthLayout';
-import RootLayout from './_root/RootLayout';
-import { Toaster } from "@/components/ui/toaster"
-
-
-
+import "./globals.css";
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import {
+  Allusers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Toaster } from "@/components/ui/toaster";
+import NotFound from "./components/shared/NotFound";
 
 const App = () => {
   return (
-    <main className='flex bg-dark-3 min-h-screen'>
+    <main className="flex bg-dark-3 min-h-screen">
       <Routes>
         {/* public routes */}
-        <Route element={<AuthLayout />} >
-          <Route path='/sign-in' element={<SigninForm />} />
-          <Route path='/sign-up' element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
-          <Route path='/explore' element={<Explore/>} />
-          <Route path='/saved' element={<Saved/>} />
-          <Route path='/all-users' element={<Allusers/>} />
-          <Route path='/create-post' element={<CreatePost/>} />
-          <Route path='/update-post/:id' element={<EditPost/>} />
-          <Route path='/posts/:id' element={<PostDetails/>} />
-          <Route path='/profile/:id/*' element={<Profile/>} />
-          <Route path='/update-profile/:id' element={<UpdateProfile/>} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<Allusers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
+          <Route path="*" element={<NotFound />} />
       </Routes>
 
-      <Toaster/>
+      <Toaster />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 import AuthProvider from './context/AuthContext'
 import { QueryProvider } from './lib/react-query/QueryProvider'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
+  <BrowserRouter>
     <QueryProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </QueryProvider>
-  </HashRouter>
+  </BrowserRouter>
 )
 
