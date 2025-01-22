3807c3a8cd9dc8fc8160b6e16051bddf34e3e5d8
zzzzshawn <zzzzshawn@gmail.com>
2025-01-22 21:27:12 +0530

    check

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ const App = () => {
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
-
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
